09af6a8ece64de549bff2eee390079b62f932038
Pedro Castro <castro.fcefyn@gmail.com>
2023-08-25 14:26:32 -0300

    Hago Login aplicando el patron Builder

--- src/test/java/starter/stepdefinitions/login/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/login/LoginStepDefinitions.java
@@ -3,6 +3,7 @@ package starter.stepdefinitions.login;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.ensure.Ensure;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +18,7 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 public class LoginStepDefinitions {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(LoginStepDefinitions.class);
+
     @Given("{word} open the demo page")
     public void i_open_the_demo_page(String user) {
         LOGGER.info("Accessing to the NavigateTo task with the user: {}", user);
@@ -25,12 +27,14 @@ public class LoginStepDefinitions {
         );
 
     }
+
     @When("I enter valid credentials")
     public void i_enter_valid_credentials() {
         when(theActorInTheSpotlight()).attemptsTo(
-                EnterCredentials.toTheFields()
+                EnterCredentials.withUsername("username").andPassword("password")
         );
     }
+
     @Then("The log in is successfully")
     public void the_log_in_is_successfully() {
         then(theActorInTheSpotlight()).attemptsTo(
--- src/test/java/starter/tasks/EnterCredentials.java
+++ src/test/java/starter/tasks/EnterCredentials.java
@@ -1,17 +1,44 @@
 package starter.tasks;
 
+import net.serenitybdd.screenplay.Actor;
 import net.serenitybdd.screenplay.Performable;
 import net.serenitybdd.screenplay.Task;
 import net.serenitybdd.screenplay.actions.Click;
 import net.serenitybdd.screenplay.actions.Enter;
 import starter.pages.DemoLoginPage;
 
-public class EnterCredentials {
-    public static Performable toTheFields() {
-        return Task.where("{0} enter credentials to the fields",
-                Enter.theValue("username").into(DemoLoginPage.USERNAME_TEXTBOX),
-                Enter.theValue("password").into(DemoLoginPage.PASSWORD_TEXTBOX),
+public class EnterCredentials implements Task {
+
+    private final String username;
+    private final String password;
+
+    public EnterCredentials(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public static LoginBuilder withUsername(String username) {
+        return new LoginBuilder(username);
+    }
+
+    @Override
+    public <T extends Actor> void performAs(T actor) {
+        actor.attemptsTo(
+                Enter.theValue(username).into(DemoLoginPage.USERNAME_TEXTBOX),
+                Enter.theValue(password).into(DemoLoginPage.PASSWORD_TEXTBOX),
                 Click.on(DemoLoginPage.SIGN_IN_BUTTON)
         );
+    }
+
+    public static class LoginBuilder {
+        private final String username;
+
+        public LoginBuilder(String username) {
+            this.username = username;
+        }
+
+        public Performable andPassword(String password) {
+            return new EnterCredentials(username, password);
+        }
     }
 }
