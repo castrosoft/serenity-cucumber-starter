ff03aca65dd534351eb714d15b35aac8014ad74d
Pedro Castro <castro.fcefyn@gmail.com>
2023-08-25 13:20:03 -0300

    Agrego un log de ejemplo en la clase LoginStepDefinitions

--- src/test/java/starter/stepdefinitions/login/LoginStepDefinitions.java
+++ src/test/java/starter/stepdefinitions/login/LoginStepDefinitions.java
@@ -3,11 +3,10 @@ package starter.stepdefinitions.login;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import net.serenitybdd.screenplay.actions.Click;
-import net.serenitybdd.screenplay.actions.Enter;
 import net.serenitybdd.screenplay.ensure.Ensure;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import starter.pages.DashboardPage;
-import starter.pages.DemoLoginPage;
 import starter.tasks.EnterCredentials;
 import starter.tasks.NavigateTo;
 
@@ -17,8 +16,10 @@ import static net.serenitybdd.screenplay.actors.OnStage.theActorInTheSpotlight;
 
 public class LoginStepDefinitions {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginStepDefinitions.class);
     @Given("{word} open the demo page")
     public void i_open_the_demo_page(String user) {
+        LOGGER.info("Accessing to the NavigateTo task with the user: {}", user);
         givenThat(theActorCalled(user)).wasAbleTo(
                 NavigateTo.theDemoLoginPage()
         );
